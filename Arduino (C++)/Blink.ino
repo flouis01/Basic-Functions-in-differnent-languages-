ddc06f54b09b8c74562acf610cba07d0ad6c6915
flouis01 <48184466+flouis01@users.noreply.github.com>
2022-08-07 18:23:18 +0200

    Update Blink.ino

--- Arduino (C++)/Blink.ino
+++ Arduino (C++)/Blink.ino
@@ -5,15 +5,15 @@
     This code makes the buildin-LED (Pin 13) Blink with a 1s delay.
 */
 
-#define LED_Buldin 13
+#define LED_Buldin 13 //Defines LED_Buildin as 13
 
 void setup(){
-  pinMode(13, OUTPUT);
+  pinMode(LED_Buildin, OUTPUT); //Sets LED_Buildin as output
 }
 
 void loop(){
-  digitalWrite(LED_Buldin, HIGH);
-  delay(1000);
-  digitalWrite(LED_Buldin, LOW);
-  delay(1000);
+  digitalWrite(LED_Buldin, HIGH); //Sets LED_Buildin High
+  delay(1000); //Waits 1000ms
+  digitalWrite(LED_Buldin, LOW); //Sets LED_Buildin LOW
+  delay(1000); //Waits 1000ms
 }
