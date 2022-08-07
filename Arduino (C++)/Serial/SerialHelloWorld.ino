677cbfa6ef1cbbe86e4db8c23c8bcd1ac0b13c1d
flouis01 <48184466+flouis01@users.noreply.github.com>
2022-08-07 18:21:13 +0200

    Create SerialHelloWorld.ino

---
+++ Arduino (C++)/Serial/SerialHelloWorld.ino
@@ -0,0 +1,11 @@
+/*
+  Name:
+    Serial Hello World
+  Discription:
+    This code sends "Hello World!" using the Serial connection. 
+*/
+
+void setup(){
+  while(Serial.begin(9600)); //Opens Serial connection and waits until its established
+  Serial.println(Hello World!); //Sends "Hello World!" 
+}
